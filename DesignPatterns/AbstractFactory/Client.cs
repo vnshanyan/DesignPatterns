06876ab1155f2057a361564d89bbdc02b8cf7bfd
Varduhi Nshanyan <varduhi.nshanyan@ameriabank.am>
2025-12-02 17:25:23 +0400

    Change comments.

--- DesignPatterns/AbstractFactory/Client.cs
+++ DesignPatterns/AbstractFactory/Client.cs
@@ -1,7 +1,7 @@
 ﻿namespace AbstractFactory;
 
-// The client code works with factories and products only through abstract types: AbstractFactory and AbstractProduct. This lets you pass any
-// factory or product subclass to the client code without breaking it.
+// The client code works with factories and products only through abstract types: AbstractFactory and AbstractProduct.
+// This lets you pass any factory or product subclass to the client code without breaking it.
 
 class Client
 {
@@ -9,11 +9,13 @@ class Client
     {
         // The client code can work with any concrete factory class.
         Console.WriteLine("Client: Testing client code with the first factory type...");
-        ClientMethod(new ConcreteFactory1());
+        ConcreteFactory1 factory1 = new ConcreteFactory1();
+        ClientMethod(factory1);
         Console.WriteLine();
 
         Console.WriteLine("Client: Testing the same client code with the second factory type...");
-        ClientMethod(new ConcreteFactory2());
+        ConcreteFactory2 factory2 = new ConcreteFactory2();
+        ClientMethod(factory2);
     }
 
     public void ClientMethod(IAbstractFactory factory)
--- DesignPatterns/AbstractFactory/IAbstractProductB.cs
+++ DesignPatterns/AbstractFactory/IAbstractProductB.cs
@@ -1,8 +1,7 @@
 ﻿namespace AbstractFactory;
 
-// Here's the the base interface of another product. All products can
-// interact with each other, but proper interaction is possible only between
-// products of the same concrete variant.
+// Here's the the base interface of another product.
+// All products can interact with each other, but proper interaction is possible only between products of the same concrete variant.
 public interface IAbstractProductB
 {
     // Product B is able to do its own thing...
@@ -10,7 +9,6 @@ public interface IAbstractProductB
 
     // ...but it also can collaborate with the ProductA.
     //
-    // The Abstract Factory makes sure that all products it creates are of
-    // the same variant and thus, compatible.
+    // The Abstract Factory makes sure that all products it creates are of the same variant and thus, compatible.
     string AnotherUsefulFunctionB(IAbstractProductA collaborator);
 }
