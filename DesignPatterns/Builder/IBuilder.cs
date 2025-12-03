8cfcce8bf43941d233731c5bf010f1237652b336
Varduhi Nshanyan <varduhi.nshanyan@ameriabank.am>
2025-12-03 15:57:49 +0400

    Understanding builder.

---
+++ DesignPatterns/Builder/IBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace Builder;
+
+// The Builder interface specifies methods for creating the different parts
+// of the Product objects.
+public interface IBuilder
+{
+    void BuildPartA();
+
+    void BuildPartB();
+
+    void BuildPartC();
+}
